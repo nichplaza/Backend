ea995074014f107b8e1543276ad200264a3451ef
nichplaza <109184041+nichplaza@users.noreply.github.com>
2022-12-23 01:20:23 -0300

    Update NpApplication.java

--- src/main/java/com/portfolio/np/NpApplication.java
+++ src/main/java/com/portfolio/np/NpApplication.java
@@ -4,6 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@ComponentScan(basePackages ="com.solution.controller,"
+		+ "+com.solution.np.service")
 public class NpApplication {
 
 	public static void main(String[] args) {
